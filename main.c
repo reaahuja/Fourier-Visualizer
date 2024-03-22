31ff46b89da5fecc37e65a8d357f1231eae082fd
endothermiic <103846051+endothermiic@users.noreply.github.com>
2024-03-22 13:08:18 -0400

    fixed audio to have all includes and use constants, added fft functions

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
-#include <math.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #define n 8         // stores size of fftArray
-#define nColours 6  // stores size of colours array (# of colours)
 #define baseXY \
   40  // stores how far the axis will be from the edge of the screen
 #define totalTicks 10
@@ -28,12 +27,13 @@
 #define HEX5_HEX4_BASE 0xFF200030
 #define SW_BASE 0xFF200040
 #define KEY_BASE 0xFF200050
+#define AUDIO_BASE 0xFF203040
 #define TIMER_BASE 0xFF202000
 #define PIXEL_BUF_CTRL_BASE 0xFF203020
-
-//Audio defintions
-// #define audioLength 5
-// #define timerValue 50000000;
+#define AUDIO_LENGTH 5
+#define TIMER_VAL 50000000;
+#define AUDIO_SAMPLES (AUDIO_LENGTH *8000)
+#define MAX_FREQUENCY 10000
 
 /*****GLOBAL VARIABLES*****/
 uint8_t image8b[] = {
@@ -12845,43 +12845,45 @@ short int
 volatile int pBufStart;
 volatile int* pCtrlPtr = (int*)PIXEL_BUF_CTRL_BASE;
 volatile int* keyPtr = (int*)KEY_BASE;
-//Audio structs
-// float leftAudio[audioLength*8000] = {0};
-// float rightAudio[audioLength*8000] = {0};
-// struct audio_t{
-//     volatile unsigned int control; 
-//     volatile unsigned char  rarc; 
-//     volatile unsigned char  ralc; 
-//     volatile unsigned char  wsrc; 
-//     volatile unsigned char  wslc; 
-//     volatile unsigned int  left; 
-//     volatile unsigned int  right;  
-// };
-// struct audio_t* const audioptr = ((struct audio_t*) 0xFF203040);
-
+struct audio_t{
+    volatile unsigned int control; 
+    volatile unsigned char  rarc; 
+    volatile unsigned char  ralc; 
+    volatile unsigned char  wsrc; 
+    volatile unsigned char  wslc; 
+    volatile unsigned int  left; 
+    volatile unsigned int  right;  
+};
+struct audio_t* const audioptr = ((struct audio_t*) AUDIO_BASE);
+float inputAudio[AUDIO_SAMPLES] = {0};
+float fftRealAudio[AUDIO_SAMPLES] = {0};
+float fftImgAudio[AUDIO_SAMPLES] = {0};
+float fftAudioMag[AUDIO_SAMPLES] = {0}; //array for y axis 
+float frequencyX[MAX_FREQUENCY] = {0}; //array for x axis
 
 /*****FUNCTION DECLARATIONS*****/
 void plot_pixel(int x, int y, short int colour);
 void clear_screen();
 void convertTo16Bit(uint8_t image8[], uint16_t image16[]);
 void drawTitlePage();
-//Audio functions
-// void microphoneRecording(audioPtr, leftAudio, rightAudio);
-// void microphoneOutput(); 
+void microphoneRecording();
+void microphoneOutput(); 
+void fftSetUp(); 
+void fftAlteration(int);
+void configTimer();
+void fft(float data_re[], float data_im[], const unsigned int N);
+void rearrange(float data_re[], float data_im[],
+               const unsigned int N);  // Sort array
+void compute(float data_re[], float data_im[],
+             const unsigned int N);  // Compute DTFT w FFT algorithm
 
 int main() {
   *(keyPtr + 3) = 0xf;  // Clear edge capture register for keys
   pBufStart = *pCtrlPtr;
   convertTo16Bit(image8b, image16b);
   drawTitlePage();
-  while (!(*(keyPtr + 3) & 0x1)){}
+  while (!(*(keyPtr + 3) & 0x1))
     ;  // Poll edge capture register; mask with bitwise and
-  //once key is pressed record audio from microphone
-  microphoneRecording();
-  //play audio on speaker for testing
-  speakerOutput();
-  //performing fft and changing arrays for drawing
-  fftSetUp();
   // Clear edge capture register and clear screen
   clear_screen();
   *(keyPtr + 3) = 0xf;
@@ -12913,4 +12915,164 @@ void clear_screen(void) {
 
 void plot_pixel(int x, int y, short int colour) {
   *(volatile short int*)(pBufStart + (y << 10) + (x << 1)) = colour;
+}
+
+
+void fftSetUp(){
+    memcpy(fftRealAudio, inputAudio, sizeof(AUDIO_SAMPLES));
+    fft(fftRealAudio, fftImgAudio,AUDIO_SAMPLES);
+    //Retrieve 20*log_base10(||weight||) for y-axis
+    for(int i = 0; i < AUDIO_SAMPLES; i++){
+        int magnitude = sqrt(pow(fftRealAudio[i], 2) + pow(fftImgAudio[i], 2));
+        fftAudioMag[i] = 20*log10(magnitude);
+    }
+    for (int i  = 0; i < MAX_FREQUENCY; i++){
+        frequencyX[i] = i*(8000/AUDIO_SAMPLES);
+    }
+}
+
+void fftAlteration(int switchValue){
+    /*
+    Task: If the user wants to alter the amount of the signal used, need to recompute FFT
+    1. Determine the new range of the audio 
+    2. Perform FFT on it to retrieve a revised copy of the audio 
+    */
+
+   //User can toggle switches to retrieve 1/2 second, so putting up all 10 keys would mean 5 seconds
+   double alteredAudioLength = (log2(switchValue)+ 1)*(0.5);
+   int audioSamplesAltered = ((int)alteredAudioLength)*8000; 
+   if (audioSamplesAltered == 0){
+        audioSamplesAltered = AUDIO_SAMPLES;
+   }
+
+   float fftRealAltered[audioSamplesAltered];
+   float fftImgAltered[audioSamplesAltered];
+   float fftAudioMagAltered[audioSamplesAltered];
+
+    fft(fftRealAltered, fftRealAltered, audioSamplesAltered);
+    //Retrieve 20*log_base10(||weight||) for y-axis
+    for(int i = 0; i < AUDIO_SAMPLES; i++){
+        int magnitude = sqrt(pow(fftRealAltered[i], 2) + pow(fftRealAltered[i], 2));
+        fftAudioMagAltered[i] = 20*log10(magnitude);
+    }
+
+
+}
+
+
+void microphoneRecording(struct audio_t* audioPtr, int *leftAudio, int *rightAudio){
+    //LEDS will go high during recording and timer will count down from 5
+    volatile int *ledPtr = (int *)(0xff200000);
+    volatile int* timerPtr = (int*)(0xFF202000);
+    volatile int* AUDIO = 0xFF203040; 
+    configTimer();
+
+    audioptr->ralc = 0; 
+    audioptr->ralc = 0; 
+    audioptr->wslc = 128; 
+    audioptr->wsrc = 128; 
+     
+    *(timerPtr) = 0;
+    *(timerPtr + 1) = 0x4;
+    int timing = AUDIO_LENGTH; 
+    for (int i = 0; (timing != 0); i++){
+        // leftAudio[i] =  audioptr->left; 
+        // rightAudio[i] = audioptr->right; 
+        int leftAudio =  audioptr->left; 
+        int rightAudio = audioptr->right;
+
+        inputAudio[i] = (leftAudio + rightAudio)/2;
+
+        *(ledPtr) = 0x3ff;
+
+        if ((*timerPtr & 0x1) == 0x1){
+            *(timerPtr) = 0;
+            *(timerPtr + 1) = 0x4;
+            timing--; 
+        }
+        i = i % AUDIO_SAMPLES;
+    }
+    *(ledPtr) = 0x0;
+}
+
+void microphoneOutput(){
+    for (int i = 0; i < AUDIO_SAMPLES; i++){
+        audioptr->left  = inputAudio[i];
+        audioptr->right = inputAudio[i];
+        // audioptr->left = leftAudio[i]; 
+        // audioptr->right = rightAudio[i]; 
+    }
+}
+
+void configTimer() {
+  volatile int* timerPtr = (int*)(0xFF202000);
+  // 100 Hz = 100 cycles / second, 1 cycle / 0.01 s
+  // 100 MHz clock on board = 100000000 cycles / second, 1000000 cycles / 0.01 s
+  int timerVal = TIMER_VAL;
+  int lowBits = timerVal & 0x0000ffff;   // get low 16b of timer value
+  int highBits = timerVal & 0xffff0000;  // get high 16b of timer value
+
+  *(timerPtr + 1) = 0x8;  // write 1 to STOP bit to stop timer (1000)
+  *(timerPtr) = 0;        // write 0 to TO bit to reset time out bit in status
+                          // register
+  *(timerPtr + 2) = lowBits;
+  *(timerPtr + 3) = highBits;
+
+  return;
+}
+
+void fft(float data_re[], float data_im[], const unsigned int N) {
+  rearrange(data_re, data_im, N);
+  compute(data_re, data_im, N);
+}
+
+void rearrange(float data_re[], float data_im[], const unsigned int N) {
+  unsigned int target = 0;
+  for (unsigned int position = 0; position < N; position++) {
+    if (target > position) {
+      const float temp_re = data_re[target];
+      const float temp_im = data_im[target];
+      data_re[target] = data_re[position];
+      data_im[target] = data_im[position];
+      data_re[position] = temp_re;
+      data_im[position] = temp_im;
+    }
+    unsigned int mask = N;
+    while (target & (mask >>= 1)) target &= ~mask;
+    target |= mask;
+  }
+}
+
+void compute(float data_re[], float data_im[], const unsigned int N) {
+  const float pi = -3.14159265358979323846;
+
+  for (unsigned int step = 1; step < N; step <<= 1) {
+    const unsigned int jump = step << 1;
+    const float step_d = (float)step;
+    float twiddle_re = 1.0;
+    float twiddle_im = 0.0;
+    for (unsigned int group = 0; group < step; group++) {
+      for (unsigned int pair = group; pair < N; pair += jump) {
+        const unsigned int match = pair + step;
+        const float product_re =
+            twiddle_re * data_re[match] - twiddle_im * data_im[match];
+        const float product_im =
+            twiddle_im * data_re[match] + twiddle_re * data_im[match];
+        data_re[match] = data_re[pair] - product_re;
+        data_im[match] = data_im[pair] - product_im;
+        data_re[pair] += product_re;
+        data_im[pair] += product_im;
+      }
+
+      // we need the factors below for the next iteration
+      // if we don't iterate then don't compute
+      if (group + 1 == step) {
+        continue;
+      }
+
+      float angle = pi * ((float)group + 1) / step_d;
+      twiddle_re = cos(angle);
+      twiddle_im = sin(angle);
+    }
+  }
 }
