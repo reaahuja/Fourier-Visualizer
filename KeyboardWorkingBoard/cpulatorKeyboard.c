5911504bb1be4c7389b392be94af4b0affd930fd
endothermiic <103846051+endothermiic@users.noreply.github.com>
2024-04-06 00:57:20 -0400

    Update cpulatorKeyboard.c changed condition to 0x8000

--- KeyboardWorkingBoard/cpulatorKeyboard.c
+++ KeyboardWorkingBoard/cpulatorKeyboard.c
@@ -298,14 +298,13 @@ void PS2_ISR( void )
 {
   	volatile int * PS2_ptr = (int *) PS2_BASE;		// PS/2 port address
 	int PS2_data, RAVAIL;
-    printf("reached");
 
 	PS2_data = *(PS2_ptr);							// read the Data register in the PS/2 port
-	RAVAIL = (PS2_data & 0xFFFF0000) >> 16;			// extract the RAVAIL field
+	RAVAIL = (PS2_data & 0x8000);			// extract the RAVAIL field
 	if (RAVAIL > 0)
 	{
 		/* always save the last three bytes received */
-		if (started){
+
 			byte1 = byte2;
 			byte2 = byte3;
 			byte3 = PS2_data & 0xFF;
@@ -361,12 +360,12 @@ void PS2_ISR( void )
            
 			//printf("another key pressed");
         
-		}
+
 		
 		if ( (byte2 == (char) 0xAA) && (byte3 == (char) 0x00) )
 			// mouse inserted; initialize sending of data
 			*(PS2_ptr) = 0xF4;
-			started = 1;
+	
 	}
 	return;
 }
